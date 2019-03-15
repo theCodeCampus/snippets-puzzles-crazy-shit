2b65760306d621277b3a1261045d42f8a2f5da61
Roman Roelofsen <romanroe@gmail.com>
2019-03-15 16:24:43 +0100

    wip

--- src/app/app.slides.ts
+++ src/app/app.slides.ts
@@ -1,8 +1,20 @@
 import {SlideComponents} from '@w11k/ngx-present';
 import {intro} from './intro';
 import {outro} from './outro';
+import {
+  WhyTypeScriptSlide1,
+  WhyTypeScriptSlide2,
+  WhyTypeScriptSlide3,
+  WhyTypeScriptSlide4,
+  WhyTypeScriptSlide5
+} from "./slides/intro_typescript";
 
 export const slides: SlideComponents = [
   intro,
+  WhyTypeScriptSlide1,
+  WhyTypeScriptSlide2,
+  WhyTypeScriptSlide3,
+  WhyTypeScriptSlide4,
+  WhyTypeScriptSlide5,
   outro,
 ];
---
+++ src/app/slides/intro_typescript.ts
@@ -0,0 +1,81 @@
+import {Component} from '@angular/core';
+
+// <tcc-code language="typescript" headline="sign_in.po.ts" [code]="testCode"></tcc-code>
+
+@Component({
+  template: `
+    <tcc-master-regular headline="Warum TypeScript?">
+      <img src="assets/images/derschrei1.jpg"/>
+    </tcc-master-regular>
+  `
+})
+export class WhyTypeScriptSlide1 {
+}
+
+
+@Component({
+  template: `
+    <tcc-master-regular headline="Warum TypeScript?">
+      <div>
+        <h3>Duck Typing</h3>
+        <ul>
+          <li>Wikipedia: "...bei dem der Typ eines Objektes nicht durch seine Klasse beschrieben wird, sondern
+            durch das Vorhandensein bestimmter Methoden oder Attribute."
+          </li>
+          <li>„Wenn ich einen Vogel sehe, der wie eine Ente läuft, wie eine Ente schwimmt und wie eine Ente
+            schnattert, dann nenne ich diesen Vogel eine Ente.“
+          </li>
+        </ul>
+      </div>
+    </tcc-master-regular>
+  `
+})
+export class WhyTypeScriptSlide2 {
+}
+
+@Component({
+  template: `
+    <tcc-master-regular headline="Warum TypeScript?">
+      <img src="assets/images/derschrei1.jpg"/>
+    </tcc-master-regular>
+  `
+})
+export class WhyTypeScriptSlide3 {
+}
+
+@Component({
+  template: `
+    <tcc-master-regular headline="Warum TypeScript?">
+      <div>
+        <img src="assets/images/derschrei2.jpg"/>
+        <div>
+        <small>DUCKOMENTA, https://www.duckomenta.de/</small>
+        </div>
+      </div>
+    </tcc-master-regular>
+  `
+})
+export class WhyTypeScriptSlide4 {
+}
+
+
+@Component({
+  template: `
+    <tcc-master-regular headline="Warum TypeScript?">
+      <div>
+        <h3>Duck Typing</h3>
+        <ul>
+          <li>Wikipedia: "...bei dem der Typ eines Objektes nicht durch seine Klasse beschrieben wird, sondern
+            durch das Vorhandensein bestimmter Methoden oder Attribute."
+          </li>
+          <li>„Wenn ich einen Vogel sehe, der wie eine Ente läuft, wie eine Ente schwimmt und wie eine Ente
+            schnattert, dann nenne ich diesen Vogel eine Ente.“
+          </li>
+        </ul>
+      </div>
+    </tcc-master-regular>
+  `
+})
+export class WhyTypeScriptSlide5 {
+}
+
